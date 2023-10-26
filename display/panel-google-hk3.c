6d8efb4ea905b07baa38e7e28241e8bf41696f7b
Ken Lin <lyenting@google.com>
2023-10-26 07:17:30 +0000

    panel: hk3: prevent blocking system notifications in critical paths
    sysfs_notify involve multiple locks so we cannot do it in critical paths
    - Remove te2 sysfs_notify
    - Move bl state and brightness sysfs_notify asychronously
    
    Bug: 303738012
    Test: Reboot successfully
    Change-Id: I39cf3a878dd8a6c526e5531f2dfe70f4350dde0a
    Signed-off-by: Ken Lin <lyenting@google.com>

--- display/panel-google-hk3.c
+++ display/panel-google-hk3.c
@@ -946,13 +946,7 @@ static void hk3_update_refresh_mode(struct exynos_panel *ctx,
 	 */
 	ctx->panel_idle_vrefresh = idle_vrefresh;
 	hk3_update_panel_feat(ctx, pmode, false);
-
-	/* TODO: (b/303738012) perform notifications asyncly for P24*/
-	/* Prevent sysfs_notify from resolution switch */
-	if (ctx->mode_in_progress == MODE_RES_AND_RR_IN_PROGRESS)
-		schedule_work(&ctx->state_notify);
-	else
-		backlight_state_changed(ctx->bl);
+	schedule_work(&ctx->state_notify);
 
 	dev_dbg(ctx->dev, "%s: display state is notified\n", __func__);
 }
@@ -1061,7 +1055,7 @@ static bool hk3_set_self_refresh(struct exynos_panel *ctx, bool enable)
 	if (pmode->exynos_mode.is_lp_mode) {
 		/* set 1Hz while self refresh is active, otherwise clear it */
 		ctx->panel_idle_vrefresh = enable ? 1 : 0;
-		backlight_state_changed(ctx->bl);
+		schedule_work(&ctx->state_notify);
 		return false;
 	}
 
